501e9bea819b3911f9f08825411761a61fb7c231
pashkas <57700158+pashkas@users.noreply.github.com>
2019-11-22 11:17:33 +0300

    настройка enviroment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "xxx",
+    authDomain: "rpgorganizer-72d0b.firebaseapp.com",
+    databaseURL: "https://rpgorganizer-72d0b.firebaseio.com",
+    projectId: "rpgorganizer-72d0b",
+    storageBucket: "rpgorganizer-72d0b.appspot.com",
+    messagingSenderId: "1019537468552"
+  }
+};
